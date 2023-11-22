c3efd1bbed0072dc01a56122cb3d6c3dd77785c0
Andy <aandreone@ses-sa.com.ar>
2023-11-22 19:22:04 -0300

    change pass

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ dotenv()
 export const config ={
      host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '2020',
     database: process.env.DB_DATABASE || 'devices'
 //     host: "127.0.0.1",
 //     user: "root",
