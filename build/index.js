258525db6bd827612c5a57180dc1c8bfe0a4afdc
Andy <aandreone@ses-sa.com.ar>
2023-11-22 18:38:02 -0300

    all

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = process.env.PORT || 3000;
-_app["default"].listen(PORT);
-console.log('Listening on port', PORT);
-
-//
