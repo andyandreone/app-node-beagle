08cc1d363bac194372521df84c838253e299e61b
Andy <aandreone@ses-sa.com.ar>
2023-11-22 20:30:48 -0300

    test

---
+++ test.js
@@ -0,0 +1,24 @@
+const b = require('bonescript');
+
+const leds = ["USR0", "USR1", "USR2", "USR3", "P9_15"];
+
+for(var i in leds) {
+    b.pinMode(leds[i], b.OUTPUT);
+}
+
+var state = b.HIGH;
+for(var i in leds) {
+    b.digitalWrite(leds[i], state);
+}
+
+setInterval(toggle, 100);
+
+function toggle() {
+    if(state == b.LOW) 
+        state = b.HIGH;
+    else
+        state = b.LOW;
+    for(var i in leds) {
+        b.digitalWrite(leds[i], state);
+    }
+}
