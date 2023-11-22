4f10caf147ae54324af2851fff1a188de7e6de95
Andy <aandreone@ses-sa.com.ar>
2023-11-22 19:17:35 -0300

    localhost change

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ dotenv()
 
 
 export const config ={
-     host: process.env.DB_HOST || 'localhost',
+     host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE || 'devices'
